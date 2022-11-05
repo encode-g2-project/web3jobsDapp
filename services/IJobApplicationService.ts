d634712f4b322d4bac033ac0177408dfe06f7d86
kvaillant <vaillant.karl.971@gmail.com>
2022-11-06 00:49:25 +0100

    feature(application): add service layer wor web3 contract interactions

--- services/IJobApplicationService.ts
+++ services/IJobApplicationService.ts
@@ -1,5 +1,28 @@
 import { ethers } from "ethers";
 
+export interface ChangeApplicationStatus {
+  applicantAddress: string;
+  jobId: string;
+  status: ApplicationStatus;
+}
+
+export const ApplicationStatuses = [
+  "screening",
+  "firstInterview",
+  "technicalTest",
+  "finalInterview",
+  "hired",
+  "rejected",
+] as const;
+
+export type ApplicationStatus = typeof ApplicationStatuses[number];
+
 export interface IJobApplicationService {
-  
+  newApplication: () => void;
+  getMyApplications: () => void;
+
+  getMyApplicants: (jobId: string) => void;
+
+  claimBounty: (jobId: string) => void;
+  changeApplicationStatus: (payload: ChangeApplicationStatus) => void;
 }
