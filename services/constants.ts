5e76884476692e0d2387eb34126652852e82c841
kvaillant <vaillant.karl.971@gmail.com>
2022-11-10 01:20:19 +0100

    change contract address

--- services/constants.ts
+++ services/constants.ts
@@ -1,4 +1,4 @@
 import web3JobsJson from "../assets/web3Jobs.json";
 
-export const CONTRACT_ADDRESS = "0x3c818d08E88B59B8bCF33BeF99277BAE501Dc64c"; // Jobs contract
+export const CONTRACT_ADDRESS = "0xd5869aC836E78254d7ADb8E87187637504059AA8"; // Jobs contract
 export const CONTRACT_ABI = web3JobsJson.abi; // Jobs contract ABI
